f122b7d2690f8acd2802455adc8374d0f5710bf9
Damian Traczuk <damiantra7@gmail.com>
2022-10-12 09:44:46 +0200

    Dodanie modyfikacji wprowadzonych przez develop_1

--- src/main/java/com/example/Lab1/Lab1Application.java
+++ src/main/java/com/example/Lab1/Lab1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lab1Application {
 
 	public static void main(String[] args) {
-
+		System.out.println("To jest robota dev2");
 		System.out.println("Start application");
 		SpringApplication.run(Lab1Application.class, args);
 	}
