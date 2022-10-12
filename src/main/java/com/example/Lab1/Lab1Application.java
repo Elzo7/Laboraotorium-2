47124662c53bb83a3d8d0dc380f3fc65a1e4d7ac
Damian Traczuk <damiantra7@gmail.com>
2022-10-12 09:50:31 +0200

    Zmodyfikowano klase main

--- src/main/java/com/example/Lab1/Lab1Application.java
+++ src/main/java/com/example/Lab1/Lab1Application.java
@@ -10,6 +10,9 @@ public class Lab1Application {
 		System.out.println("To jest robota dev2");
 		System.out.println("Start application");
 		System.out.println("Test Laboratorium 1");
+
+		System.out.println("Jestem konfliktowy 2");
+
 		SpringApplication.run(Lab1Application.class, args);
 	}
 
